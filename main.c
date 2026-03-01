ddec0d147b2f323dfcafd4cdfb733a059036ac43
KamilMalicki <146217192+KamilMalicki@users.noreply.github.com>
2026-03-01 16:10:32 +0100

    Fix copyright formatting in main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright [2026] [KamilMalicki]
+ * Copyright 2026 KamilMalicki
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
